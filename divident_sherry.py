642b13e86282c6963a6692542a2ec84da8e4f3bf
hochon <hochon0829@gmail.com>
2019-03-21 06:14:42 -0700

    Update divident_sherry.py

--- divident_sherry.py
+++ divident_sherry.py
@@ -2,6 +2,7 @@ import os
 from dfply import *
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
 os.chdir(r"c:\Users\ChonWai\Desktop\quantitive_trading\final_assignment")
 
 
@@ -23,11 +24,12 @@ mse.columns = [i.lower() for i in mse.columns]
 mse["exdt"] = pd.to_datetime(mse["exdt"], format='%Y%m%d')
 mse["dclrdt"] = pd.to_datetime(mse["dclrdt"], format='%Y%m%d')
 mse["paydt"] = pd.to_datetime(mse["paydt"], format='%Y%m%d')
-mse = mse >> mask(X.divamt == X.divamt) ## dropna subset == divamt
+mse = mse >> mask(X.divamt == X.divamt)
 mse["date"] = [end_of_month(date) for date in mse["exdt"]]
 # merge mse and msf
 data = pd.merge(msf, mse, how="outer", on=["permno", "date"])
 data["mcap"] = data["prc"] * data["shrout"]
+data = data >> arrange(X.permno, X.date)
 # fill na with distcd if the stock has distcd before so that we can identify stock that pay dividend
 # and stock that don't pay
 data["distcd"] = data.groupby("permno")["distcd"].transform(lambda x: x.fillna(method="ffill"))
@@ -35,8 +37,10 @@ data["distcd"] = data.groupby("permno")["distcd"].transform(lambda x: x.fillna(m
 data["divtype"] = [i if i != i else str(i)[2] for i in data["distcd"]]
 # take out the thrid digit of the dist code which represent the how often a company pay dividend
 data["disttype"] = [i if i != i else str(i)[:2] for i in data["distcd"]]
-data = data >> arrange(X.permno, X.date)
 data["prc_lag"] = data.groupby("permno")["prc"].shift(1)
+data = data.assign(div_lag3=data.groupby(["permno"])["divamt"].shift(3),
+                   div_lag6=data.groupby(["permno"])["divamt"].shift(6),
+                   div_lag12=data.groupby(["permno"])["divamt"].shift(12))
 # data set with stock that pay div and stock that don;t pay (divtype = nan)
 data = data.query('date <= "2011-12-31" &'
                   'shrcd in([10,11]) &'
@@ -46,7 +50,11 @@ data = data.query('date <= "2011-12-31" &'
                   '(disttype == "12" | disttype != disttype) &'
                   '(divtype in(["0","1","3","4","5"])| divtype != divtype)')
 data['turnover'] = data['vol']/data['shrout']
-data['spread_2'] = data['ask'] -data['bid']
+data['spread_2'] = data['ask'] - data['bid']
+data["ret"] =data.ret.apply(float)
+
+# table 1
+"""
 regular_div = data.query('divtype == divtype')
 table1_first_part = regular_div[["mcap", "spread_2", "permno","turnover"]].describe()
 table1_first_part = table1_first_part.transpose()
@@ -58,12 +66,45 @@ table1_second_part = table1_second_part.rename(index={0: "Number of Firms months
 table1 = pd.concat((table1_first_part, table1_second_part), axis=0)
 table1 = table1.rename(index={0: "Number of Firm Month", 1: "Number of Firm"},
                        columns={"count": "N"})
-data['freq'] = data.distcd.apply(lambda x:str(x)[:3])
-divData = data.dropna(subset = ['freq'])
-## bid_ask spread
-divData_spread = Data.dropna(subset = ['spread'])
-divData_spread.groupby('permno')['spread'].mean()
-divData_spread['spread']
+"""
+# Portfilio within Companies
+# filter out stock that did not give dividend
+portfilio1_data = data.query('disttype == disttype & '
+                             'date > "1927-12-31" &'
+                             'ret == ret')
+
+long = portfilio1_data.query('(divtype == "3" & div_lag3 == div_lag3) |'
+                             '(divtype == "4" & div_lag6 == div_lag6) |'
+                             '(divtype == "5" & div_lag12 == div_lag12)')
+long = pd.DataFrame({"ret": long.groupby(["date"])["ret"].apply(mean)})
+long['cumulative_ret'] = np.cumprod(long["ret"]+1)
+short = portfilio1_data.query('(divtype == "3" & div_lag3 != div_lag3) |'
+                              '(divtype == "4" & div_lag6 != div_lag6) |'
+                              '(divtype == "5" & div_lag12 != div_lag12)')
+short = pd.DataFrame({"ret": short.groupby(["date"])["ret"].apply(mean)})
+short['cumulative_ret'] = np.cumprod(short["ret"]+1)
+portfilio1 = pd.DataFrame({"ret": long.ret-short.ret})
+
+port = portfilio1_data
+port["signal"] = np.where(((port.div_lag3 == port.div_lag3) & (port.divtype == "3")) |
+                          ((port.div_lag6 == port.div_lag6) & (port.divtype == "4")) |
+                          ((port.div_lag12 == port.div_lag12) & (port.divtype == "5")), "L",
+                          np.where(((port.div_lag3 != port.div_lag3) & (port.divtype == "3")) |
+                          ((port.div_lag6 != port.div_lag6) & (port.divtype == "4")) |
+                          ((port.div_lag12 != port.div_lag12) & (port.divtype == "5")), "S", ""))
+port["ewret"] = port.groupby(["date", "signal"])["ret"].transform("mean")
+portfilio2 = pd.DataFrame({"ret": port.query('signal in(["L","S"])').groupby(
+                                 ["date"]).apply(lambda x: sum(np.where(x.signal == "L",
+                                                                        x.ewret, x.ewret*-1)))})
+
+portfilio1['cumulative_ret'] = np.cumprod(portfilio1['ret']+1)
+portfilio2['cumulative_ret'] = np.cumprod(portfilio2['ret']+1)
+x = portfilio1.index
+y = portfilio1.cumulative_ret
+plt.plot(x, y, color='coral')
+plt.show()
+
+
 
 
 
